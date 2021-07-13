4b00d232bf0ede852a9bf0734e23a8b3095086b4
mudabbir-ali-tx <mudabbir.ali@techcreatix.com>
2021-07-13 19:45:49 +0500

    ngin

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,10 +3,3 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
-
-if (process.env.NODE_ENV) {
-  app.use(express.static(path.resolve(process.cwd(), 'client/build')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(process.cwd(), 'client/build/index.html'))
-  })
-}
